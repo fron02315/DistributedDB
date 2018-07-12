42cb71bf9bedeee1b02c758fd9e4e5e43d7d04ec
PengprondR <33078510+fron02315@users.noreply.github.com>
2018-07-13 00:28:37 +0800

    Add files via upload

--- blockchain/Blockchain-clientside.go
+++ blockchain/Blockchain-clientside.go
@@ -11,18 +11,18 @@ import (
 // Block keeps block headers
 type Block struct {
   Index int
-	Timestamp     int64
+	Timestamp     string
   Nodeid1    string
   Nodeid2    string
+  Operation   string
   MsgSize       string
 	PrevBlockHash []byte
 	Hash          []byte
 }
 
 func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
   index := []byte(strconv.Itoa(b.Index))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, index, []byte(b.Nodeid1), []byte(b.Nodeid2), []byte(b.MsgSize), timestamp}, []byte{})
+	headers := bytes.Join([][]byte{b.PrevBlockHash, index, []byte(b.Operation), []byte(b.Nodeid1), []byte(b.Nodeid2), []byte(b.MsgSize), []byte(b.Timestamp)}, []byte{})
 	hash := sha256.Sum256(headers)
 
 	b.Hash = hash[:]
@@ -41,17 +41,19 @@ func isBlockValid(newBlock Block, oldBlock Block) bool {
 }
 
 // NewBlock creates and returns Block
-func NewBlock(Nodeid1 string, Nodeid2 string, MsgSize string, oldBlock *Block) *Block {
+func NewBlock(Nodeid1 string, Nodeid2 string, Operation string, MsgSize string, oldBlock *Block) *Block {
   newIndex := oldBlock.Index+1
-	block := &Block{newIndex, time.Now().Unix(), Nodeid1, Nodeid2, MsgSize, oldBlock.Hash, []byte{}}
+	t := time.Now()
+	block := &Block{newIndex, t.String(), Nodeid1, Nodeid2, Operation, MsgSize, oldBlock.Hash, []byte{}}
 	block.SetHash()
 	return block
 }
 
 // NewGenesisBlock creates and returns genesis Block
 func NewGenesisBlock() *Block {
+	t := time.Now()
   //To begin the new block, we assign the index=0 and all elements are null or zero
-  genesisBlock := &Block{0, time.Now().Unix(), "0", "0", "0", []byte(""), []byte{}}
+  genesisBlock := &Block{0, t.String(), "0", "0", "null", "0", []byte(""), []byte{}}
   genesisBlock.SetHash()
 	return genesisBlock
 }
@@ -61,11 +63,11 @@ type Blockchain struct {
 }
 
 // AddBlock saves provided data as a block in the blockchain
-func AddBlock(bc *Blockchain, Nodeid1 string, Nodeid2 string, MsgSize string) {
+func AddBlock(bc *Blockchain, Nodeid1 string, Nodeid2 string, Operation string, MsgSize string) {
   //old block
 	prevBlock := bc.Blocks[len(bc.Blocks)-1]
 
-	newBlock := NewBlock(Nodeid1, Nodeid2, MsgSize, prevBlock)
+	newBlock := NewBlock(Nodeid1, Nodeid2, Operation, MsgSize, prevBlock)
 	bc.Blocks = append(bc.Blocks, newBlock)
 }
 
@@ -78,9 +80,11 @@ func PrintBC(bc *Blockchain){
   for _, block := range bc.Blocks {
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
     fmt.Printf("Block index: %d\n", block.Index)
+		fmt.Printf("Timestamp: %s\n", block.Timestamp)
 		fmt.Printf("Node ID1: %s\n", block.Nodeid1)
     fmt.Printf("Node ID2: %s\n", block.Nodeid2)
-    fmt.Printf("Message size: %s\n", block.MsgSize)
+    fmt.Printf("Operation performed: %s\n", block.Operation)
+    fmt.Printf("Message size (Bytes): %s\n", block.MsgSize)
 		fmt.Printf("Hash: %x\n", block.Hash)
 		fmt.Println()
 	}
